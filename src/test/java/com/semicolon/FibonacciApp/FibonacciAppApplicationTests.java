0ffaaeee90b36c9ed661a72a2333820f92be01b7
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-06-13 08:57:12 +0100

    feat: Initial commit

---
+++ src/test/java/com/semicolon/FibonacciApp/FibonacciAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.FibonacciApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/semicolon/FibonacciApp/UserTest.java
@@ -0,0 +1,23 @@
+package com.semicolon.FibonacciApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class UserTest {
+
+    @Autowired
+    private UserService userService;
+    @Test
+    void registerUser() {
+        UserRequest userRequest = new UserRequest();
+        userRequest.setFirstName("test");
+        userRequest.setLastName("user");
+        userRequest.setEmail("test@email.com");
+
+        UserResponse response = userService.register(userRequest);
+        assertTrue(response.isSuccess());
+    }
+}
